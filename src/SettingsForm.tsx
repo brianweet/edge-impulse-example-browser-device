f9f65588066c3181f6a9dfaf3aca155781943096
Brian Weeteling <brianweeteling@gmail.com>
2020-03-03 22:33:59 +0100

    Ask for DeviceMotion permissions

--- src/SettingsForm.tsx
+++ src/SettingsForm.tsx
@@ -7,19 +7,48 @@ interface SettingsFormProps {
 }
 export class SettingsForm extends React.Component<
   SettingsFormProps,
-  EdgeImpulseSettings
+  EdgeImpulseSettings & { error: string }
 > {
   constructor(props: SettingsFormProps) {
     super(props);
     this.state = {
-      apiKey: "",
+      apiKey: getApiKey(),
       device: {
         deviceId: getDeviceId(),
         deviceType: "BROWSER_CLIENT",
-        accelerometerInterval: 16
-      }
+        accelerometerInterval: 10
+      },
+      error: ""
     };
+    this.initAccelerometer();
   }
+
+  initAccelerometer() {
+    if (
+      typeof DeviceMotionEvent !== "undefined" &&
+      typeof (DeviceMotionEvent as any).requestPermission === "function"
+    ) {
+      (DeviceMotionEvent as any)
+        .requestPermission()
+        .then((response: string) => {
+          if (response == "granted") {
+            this.setState({
+              error: ""
+            });
+          } else {
+            this.setState({
+              error: "Device motion permissions denied"
+            });
+          }
+        })
+        .catch(() => {
+          this.setState({
+            error: "Failed to request device motion permissions"
+          });
+        });
+    }
+  }
+
   handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
     this.setState({
@@ -42,6 +71,7 @@ export class SettingsForm extends React.Component<
     return (
       <form
         onSubmit={e => {
+          e.preventDefault();
           this.props.updateSettings(this.state);
         }}
       >
@@ -79,8 +109,18 @@ export class SettingsForm extends React.Component<
             disabled
           />
         </label>
-
-        <input type="submit" value="Submit" />
+        {this.state.error ? (
+          <button
+            onClick={e => {
+              e.preventDefault();
+              this.initAccelerometer();
+            }}
+          >
+            Request permissions
+          </button>
+        ) : (
+          <input type="submit" value="Submit" />
+        )}
       </form>
     );
   }
