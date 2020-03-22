5b14524c2e54d669aef0806d6874ccfefc1af8ab
Brian Weeteling <brianweeteling@gmail.com>
2020-03-22 08:52:05 +0100

    Revert "Test build error"
    This reverts commit 16bc6b6c4c58bf81fa9ec77857cb12f8259ec60e.

--- client/settings.ts
+++ client/settings.ts
@@ -1,28 +1,29 @@
-const REMOTE_MANAGEMENT_ENDPOINT = "wss://remote-mgmt.edgeimpulse.com";
-const INGESTION_API = "https://ingestion.edgeimpulse.com";
-const LS_API_KEY = "apiKey";
-const LS_DEVICE_ID_KEY = "deviceId";
+const REMOTE_MANAGEMENT_ENDPOINT = 'wss://remote-mgmt.edgeimpulse.com';
+const INGESTION_API = 'https://ingestion.edgeimpulse.com';
+const LS_API_KEY = 'apiKey';
+const LS_DEVICE_ID_KEY = 'deviceId';
 
-const getRandomString = () => Date.now().toString(36);
+const getRandomString = () =>
+    Date.now().toString(36);
 
-export const getApiKeyz = () =>
-  new URLSearchParams(window.location.search).get("apiKey") ||
-  localStorage.getItem(LS_API_KEY) ||
-  "";
+export const getApiKey = () =>
+    new URLSearchParams(window.location.search).get('apiKey') ||
+    localStorage.getItem(LS_API_KEY) ||
+    '';
 export const storeApiKey = (apiKey: string) => {
-  localStorage.setItem(LS_API_KEY, apiKey);
+    localStorage.setItem(LS_API_KEY, apiKey);
 };
 
 export const getDeviceId = () =>
-  localStorage.getItem(LS_DEVICE_ID_KEY) || `phone_${getRandomString()}`;
+    localStorage.getItem(LS_DEVICE_ID_KEY) || `phone_${getRandomString()}`;
 export const storeDeviceId = (deviceId: string) => {
-  localStorage.setItem(LS_DEVICE_ID_KEY, deviceId);
+    localStorage.setItem(LS_DEVICE_ID_KEY, deviceId);
 };
 
 export const getIngestionApi = () =>
-  new URLSearchParams(window.location.search).get("ingestionApi") ||
-  INGESTION_API;
+    new URLSearchParams(window.location.search).get('ingestionApi') ||
+    INGESTION_API;
 
 export const getRemoteManagementEndpoint = () =>
-  new URLSearchParams(window.location.search).get("remoteManagement") ||
-  REMOTE_MANAGEMENT_ENDPOINT;
+    new URLSearchParams(window.location.search).get('remoteManagement') ||
+    REMOTE_MANAGEMENT_ENDPOINT;
